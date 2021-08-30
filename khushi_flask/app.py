3afa3b09c726baf3e714115616f8ab08e597dea6
khushi kaur <khushik16001@gmail.com>
2021-08-30 14:06:45 +0530

    khushi submit

---
+++ khushi_flask/app.py
@@ -0,0 +1,25 @@
+from flask import Flask,request,render_template,redirect
+
+app=Flask(__name__,template_folder='template')
+
+#routes
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        username=request.form.get('username')
+        phone=request.form.get('phone')
+        print(username)
+        print(phone)
+        return redirect('/')
+    return render_template('login.html')
+    
+if __name__=="__main__":
+    app.run(debug=True)
