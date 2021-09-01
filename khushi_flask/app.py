39325a7deb014ca2ff78050939e715c22a686398
khushi kaur <khushik16001@gmail.com>
2021-09-01 13:24:56 +0530

    khushichange

--- khushi_flask/app.py
+++ khushi_flask/app.py
@@ -3,23 +3,29 @@ from flask import Flask,request,render_template,redirect
 app=Flask(__name__)
 
 #routes
-@app.route("/")
-def home():
+
+@app.route("/",methods=['GET','POST'])
+def form():
+    if request.method=='POST':
+        name=request.form.get('name')
+        phone=request.form.get('phone')
+        email=request.form.get('email')
+        age=request.form.get('age')
+        stream=request.form.get('stream')
+        address=request.form.get('address')
+        print(name)
+        print(phone)
+        print(email)
+        print(age)
+        print(stream)
+        print(address)
+        return render_template('result.html',username=name,phone=phone,email=email,age=age,stream=stream,address=address)
+    return render_template('forms.html')
+
+@app.route("/index")
+def index():
     return render_template('index.html')
 
-@app.route("/about")
-def about():
-    return render_template('about.html')
-
-@app.route("/login",methods=['GET','POST'])
-def login():
-    if request.method=='POST':
-        username=request.form.get('username')
-        phone=request.form.get('phone')
-        print(username)
-        print(phone)
-        return redirect('/')
-    return render_template('login.html')
 
 # this is sid's comment  
 
