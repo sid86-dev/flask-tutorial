58292c6c3bf960a506f370c5dd17d84f15e1aba6
tithigupta25 <tithigupta2001@gmail.com>
2021-09-05 22:07:52 +0530

    tithi_flask

---
+++ Students/tithi_flask/day3/app.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template
+from file_read_tithi import read_data
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    data=read_data()
+    process_data = data.split("$")
+    length=len(process_data)
+    print(process_data)
+    print(length)
+    return render_template('index.html',process_data=process_data,length=length)
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ Students/tithi_flask/day3/test.py
@@ -0,0 +1,8 @@
+s="this is a header$this is a task"
+
+split = s.split("$")
+
+for item in split:
+    print(split.index(items))
+    print(item)
+    print(len(split))
