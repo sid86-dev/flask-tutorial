3cdc31600ab1e12dd3974790faf592736aa16f88
poojahukkire <hukkirepooja1994@gmail.com>
2021-09-07 11:13:48 +0530

    pooja_commit

---
+++ Students/pooja_flask/day3/file_read_pooja.py
@@ -0,0 +1,6 @@
+def read_data(): 
+     f = open('todo.txt','r') 
+      
+     data=f.read() 
+     f.close() 
+     return data 
---
+++ Students/pooja_flask/day3/test.py
@@ -0,0 +1,10 @@
+s="this is a header$this is a task" 
+
+ 
+split = s.split("$") 
+ 
+ 
+for item in split: 
+     print(split.index(items)) 
+     print(item) 
+     print(len(split)) 
