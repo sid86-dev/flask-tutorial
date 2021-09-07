dd7b988c7d06efe61837381694e5487d6bde24c3
blog <ssureshkumaryadav7@gmail.com>
2021-09-07 20:42:34 +0530

    suresh_flask_claculor

---
+++ Students/suresh_flask/day1/day4.py
@@ -0,0 +1,14 @@
+from flask import Flask ,render_template
+from file_read import read_data
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    data=read_data()
+    process_data = data.split("$")
+    length = len(process_data)  
+    return render_template('index.html', process_data=process_data,length=length)
+
+if __name__ == '__main__':
+    app.run(debug=True)
