7a20c3ac7c6cfdcba2006e380cfab9c856e56fcc
khushi <khushik16001@gmail.com>
2021-09-08 18:13:15 +0530

    khushi_perform

--- Students/khushi_flask/day2/redirection.py
+++ Students/khushi_flask/day2/redirection.py
@@ -1,18 +1,21 @@
-from flask import Flask,redirect,render_template,session
+from flask import Flask, render_template,redirect,request
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/process',methods=['GET','POST'])
+def process():
+    if request.method=='POST':
+        name = request.form.get("get_name")
+        return redirect(f'/about/{name}')
+    return "error"
+
 @app.route('/about/<string:name>')
 def about(name):
-    return render_template('about.html')
-
-@app.route('/process')
-def process():
-    return render_template("/")
-
-if __name__=="main":
+    return render_template('about.html',name=name)
+    
+if __name__=="__main__":
     app.run(debug=True)
